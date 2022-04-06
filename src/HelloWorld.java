ad17e460f2a43ac35f7e8b06d99644d8286d02c2
xilieme <103028399+xilieme@users.noreply.github.com>
2022-04-06 15:36:55 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@
 public class HelloWorld{
 
 	public static void main(String[] args){
-		System.out.println("-----Hello World & Welcome to Jenkins Training-----");
+		System.out.println("-----Hello World Made somes changes-----");
 	}
 }
