9ca337c22b3cca4428166c91d37d44fd3019e805
xilieme <103028399+xilieme@users.noreply.github.com>
2022-04-06 15:42:51 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@
 public class HelloWorld{
 
 	public static void main(String[] args){
-		System.out.println("-----Hello World Made somes new others changes-----");
+		System.out.println("-----Hello World Made somes new  changes-----");
 	}
 }
